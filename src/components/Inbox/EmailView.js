4581c12e93f18c6a27629f01e42ddbc5cbd4fef4
Arivoli A.E <aehari2010@gmail.com>
2019-03-01 23:33:08 -0800

    UI changes

--- src/components/Inbox/EmailView.js
+++ src/components/Inbox/EmailView.js
@@ -1,57 +1,51 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
-class EmailView extends Component{
-    constructor(props){
+class EmailView extends Component {
+    constructor(props) {
         super(props);
-        console.log('iddd'+ this.props.emailId);
+        console.log('iddd' + this.props.emailId);
 
         this.state = {
             emailAddress: "",
             subject: "",
-            content: ""
+            text: ""
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
 
-        axios.get('http://5c5a21f9af3ff700140de477.mockapi.io/api/email/'+this.props.emailId)
-            .then((response)=>{
-                if(response.status === 200){
+        axios.get('http://5c5a21f9af3ff700140de477.mockapi.io/api/email/' + this.props.emailId)
+            .then((response) => {
+                if (response.status === 200) {
                     this.setState({
                         emailAddress: response.data.to,
                         subject: response.data.subject,
-                        content: response.data.text
+                        text: response.data.text
                     })
                 }
             });
     }
 
-    
-    
-   
+    render() {
 
-    render(){
-
-
-    
-        return(
+        return (
             <div>
-                 <div className="pt-3 pb-3 ml-5">
+                <div className="pt-3 pb-3 ml-5">
                     <p>To: {this.state.emailAddress}</p>
                 </div>
-                <hr/>
+                <hr />
                 <div className="pt-3 pb-3 ml-5">
                     <p>Subject: {this.state.subject}</p>
                 </div>
-                <hr/>
+                <hr />
                 <div className="pt-3 pb-3 ml-5">
                     <div>
-                   {this.state.content}
+                        {this.state.text}
                     </div>
                 </div>
                 <div className="flt-right mr-5">
-                <button className="btn-lg btn-info mr-2">Prev</button>
+                    <button className="btn-lg btn-info mr-2">Prev</button>
                     <button className="btn-lg btn-primary">Next</button>
                 </div>
             </div>
--- src/components/Inbox/ListView.js
+++ src/components/Inbox/ListView.js
@@ -17,7 +17,7 @@ class ListView extends Component {
 
     componentDidMount(){
         //axios.defaults.withCredentials=true;
-        for(var i=1;i<=5;i++){
+        for(var i=1;i<=10;i++){
             axios.get('http://5c5a21f9af3ff700140de477.mockapi.io/api/email/'+i)
             .then((response)=>{
                 
@@ -63,28 +63,10 @@ class ListView extends Component {
         return (
             <div>
                 {mailListContent}
-                {/* <div className="row mt-2" onClick={this.props.toggleEmailView}>
-                    <div className="col-3">
-                        <div className="container">
-                            <div><input type="checkbox" className="ml-2 mr-3 chkbox" /> xyz@gmail.com</div>
-                        </div>
-                    </div>
-                    <div className="col-9">
-                        content of email
-                    </div>
+                <div className="flt-right mr-5 mt-5">
+                <button className="btn-lg btn-danger">Delete</button>
+
                 </div>
-                <hr />
-                <div className="row mt-2">
-                    <div className="col-3">
-                        <div className="container">
-                            <div><input type="checkbox" className="ml-2 mr-3 chkbox" /> xyz@gmail.com</div>
-                        </div>
-                    </div>
-                    <div className="col-9">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                    </div>
-                </div>
-                <hr /> */}
             </div>
         )
     }
